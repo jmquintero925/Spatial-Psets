aa78e9f94c25b737493f3d44ede06d64182d868c
Jose Quintero <jose.miguel.quintero@gmail.com>
2023-03-07 18:58:11 -0600

    Function that identifies cells from USA

---
+++ Pset2/code/DataExtraction.m
@@ -0,0 +1,7 @@
+%% Code that gets the data
+clear all; close all; clc;
+
+% Set the path for data
+oDataPath = '../../../EGGW_Replication_Package/Data/';
+
+idx = usaExtract();
---
+++ Pset2/code/usaExtract.m
@@ -0,0 +1,28 @@
+function out = usaExtract()
+
+% Load set of coordinates 
+load('../data/usa_coord.mat')
+% Create matrices of long and lat 
+lat = repmat(flip(-89.5:89.5)',1,360);
+long = repmat(-179.5:179.5,180,1);
+
+% Loop over USA coordinates
+N = size(usa,1);
+X = [lat(:),long(:)];
+
+
+
+% Extract vector
+out = zeros(size(lat(:)));
+
+% Loop getting 
+for j= 1:N
+    % Find indices that minimize
+    [~,I] = min(sum((X-usa(j,:)).^2,2));
+    % Replace on out vector as part of USA
+    out(I) = true;
+end
+
+out = reshape(out,180,360);
+
+end
